bb5512b7d8220b03cda7395e7079648c529addde
howardliu-github <howard.d.liu@gmail.com>
2021-05-01 16:48:32 -0700

    second commit

--- travels/planner/urls.py
+++ travels/planner/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     # path('dashboard', views.get_all, name='dashboard'),
     path('', views.index),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
     path('myaccount/<int:user_id>', views.edit),
     path('myaccount/<int:user_id>/update', views.update),
 ]
